34372fc8e228d8ed736383eb6a165c18be22c5fd
dangoss76 <dangoss.perso@gmail.com>
2024-09-19 16:12:30 -0400

    pytest  4

--- tests/quotes.py
+++ tests/quotes.py
@@ -13,46 +13,47 @@ import time
 @pytest.fixture()
 def test1(browser):
 
-    print("Test Execution Started")
+print("Test Execution Started")
 
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-ssl-errors=yes')
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument("--headless=new")  # for Chrome >= 109
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("--headless=new")  # for Chrome >= 109
 
-    browser = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
-        options=options
-    )
+browser = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    options=options
+)
 
-    # Open Browser
-    # browser = webdriver.Firefox()
-    # Navigate to TMX
-    browser.get("https://www.m-x.ca/en/trading/data/quotes")
+# Open Browser
+# browser = webdriver.Firefox()
 
-    # Defining the columns to read
-    quoteOptions = ['symbolOEQ', 'symbolETF', 'symbolSSF']  # Select name
-    quoteCSV = ['csv/equity.csv', 'csv/etf.csv', 'csv/shareFutures.csv']  # CSV fie
+# Navigate to TMX
+browser.get("https://www.m-x.ca/en/trading/data/quotes")
 
-    for q1, q2 in zip(quoteOptions, quoteCSV):
+# Defining the columns to read
+quoteOptions = ['symbolOEQ', 'symbolETF', 'symbolSSF']  # Select name
+quoteCSV = ['csv/equity.csv', 'csv/etf.csv', 'csv/shareFutures.csv']  # CSV fie
 
-        data = pd.read_csv(f'{q2}')
+for q1, q2 in zip(quoteOptions, quoteCSV):
 
-        for index, row in data.iterrows():
-            # element = browser.find_element(By.ID,'page-content') # Find the top element of the page
-            # browser.execute_script("arguments[0].scrollIntoView();", element) # We scroll back to top
-            browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + Keys.HOME)
-            dropdown_equity = browser.find_element(By.ID, (f'{q1}'))  # we select a quotes in the dropdown
-            select = Select(dropdown_equity)
-            select.select_by_value(row['symbol'])
+    data = pd.read_csv(f'{q2}')
 
-            clickOkBtn = browser.find_element(By.XPATH,
-                                              "//select[@id='" + f'{q1}' + "']/following-sibling::div").click()  # Click OK button next to the select
+    for index, row in data.iterrows():
+        # element = browser.find_element(By.ID,'page-content') # Find the top element of the page
+        # browser.execute_script("arguments[0].scrollIntoView();", element) # We scroll back to top
+        browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + Keys.HOME)
+        dropdown_equity = browser.find_element(By.ID, (f'{q1}'))  # we select a quotes in the dropdown
+        select = Select(dropdown_equity)
+        select.select_by_value(row['symbol'])
 
-            quoteResult = browser.find_element(By.TAG_NAME, 'h2').text  # Validate if title match
-            assert quoteResult == row['title']
+        clickOkBtn = browser.find_element(By.XPATH,
+                                          "//select[@id='" + f'{q1}' + "']/following-sibling::div").click()  # Click OK button next to the select
 
-            print(row['symbol'], row['title'])
+        quoteResult = browser.find_element(By.TAG_NAME, 'h2').text  # Validate if title match
+        assert quoteResult == row['title']
 
-    browser.quit()
-    print("Test Execution Successfully Completed!")
+        print(row['symbol'], row['title'])
+
+browser.quit()
+print("Test Execution Successfully Completed!")

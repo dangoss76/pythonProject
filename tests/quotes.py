21ca9b2816b46106fd9c09f2ee4cb08e725ab57f
dangoss76 <dangoss.perso@gmail.com>
2024-09-20 08:14:48 -0400

    pytest  12

--- tests/quotes.py
+++ tests/quotes.py
@@ -1,4 +1,3 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,62 +9,47 @@ import pandas as pd
 import csv
 import time
 
-#Fixture for Chrome
-@pytest.fixture(scope="class")
-def chrome_driver_init(request):
-    chrome_driver = webdriver.Chrome()
-    request.cls.driver = chrome_driver
-    yield
-    chrome_driver.close()
+print("Test Execution Started")
 
-@pytest.mark.usefixtures("chrome_driver_init")
-class BasicTest:
-    pass
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("--headless=new")  # for Chrome >= 109
 
-class Test_Quote(BasicTest):
-    def test_Quote(self):
+browser = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    options=options
+)
 
-        print("Test Execution Started")
+# Open Browser
+# browser = webdriver.Firefox()
 
-        #options = webdriver.ChromeOptions()
-        #options.add_argument('--ignore-ssl-errors=yes')
-        #options.add_argument('--ignore-certificate-errors')
-        #options.add_argument("--headless=new")  # for Chrome >= 109
+# Navigate to TMX
+browser.get("https://www.m-x.ca/en/trading/data/quotes")
 
-       # self.browser = webdriver.Remote(
-           # command_executor='http://localhost:4444/wd/hub',
-           # options=options
-        #)
+# Defining the columns to read
+quoteOptions = ['symbolOEQ', 'symbolETF', 'symbolSSF']  # Select name
+quoteCSV = ['equity.csv', 'etf.csv', 'shareFutures.csv']  # CSV fie
 
-        # Open Browser
-        # browser = webdriver.Firefox()
+for q1, q2 in zip(quoteOptions, quoteCSV):
 
-        # Navigate to TMX
-        self.browser.get("https://www.m-x.ca/en/trading/data/quotes")
+    data = pd.read_csv(f'{q2}')
 
-        # Defining the columns to read
-        quoteOptions = ['symbolOEQ', 'symbolETF', 'symbolSSF']  # Select name
-        quoteCSV = ['csv/equity.csv', 'csv/etf.csv', 'csv/shareFutures.csv']  # CSV fie
+    for index, row in data.iterrows():
+        # element = browser.find_element(By.ID,'page-content') # Find the top element of the page
+        # browser.execute_script("arguments[0].scrollIntoView();", element) # We scroll back to top
+        browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + Keys.HOME)
+        dropdown_equity = browser.find_element(By.ID, (f'{q1}'))  # we select a quotes in the dropdown
+        select = Select(dropdown_equity)
+        select.select_by_value(row['symbol'])
 
-        for q1, q2 in zip(quoteOptions, quoteCSV):
+        clickOkBtn = browser.find_element(By.XPATH,
+                                          "//select[@id='" + f'{q1}' + "']/following-sibling::div").click()  # Click OK button next to the select
 
-            data = pd.read_csv(f'{q2}')
+        quoteResult = browser.find_element(By.TAG_NAME, 'h2').text  # Validate if title match
+        assert quoteResult == row['title']
 
-            for index, row in data.iterrows():
-                # element = browser.find_element(By.ID,'page-content') # Find the top element of the page
-                # browser.execute_script("arguments[0].scrollIntoView();", element) # We scroll back to top
-                self.browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + Keys.HOME)
-                dropdown_equity = self.browser.find_element(By.ID, (f'{q1}'))  # we select a quotes in the dropdown
-                select = Select(dropdown_equity)
-                select.select_by_value(row['symbol'])
+        print(row['symbol'], row['title'])
 
-                clickOkBtn = self.browser.find_element(By.XPATH,
-                                                  "//select[@id='" + f'{q1}' + "']/following-sibling::div").click()  # Click OK button next to the select
-
-                quoteResult = self.browser.find_element(By.TAG_NAME, 'h2').text  # Validate if title match
-                assert quoteResult == row['title']
-
-                print(row['symbol'], row['title'])
-
-        self.browser.quit()
-        print("Test Execution Successfully Complpeted!")
+browser.quit()
+print("Test Execution Successfully Completed!")

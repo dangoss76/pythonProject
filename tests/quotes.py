9e911ed3f69adc07319414790d8da527618100f4
dangoss76 <dangoss.perso@gmail.com>
2024-09-20 09:06:18 -0400

    pytest  15

--- tests/quotes.py
+++ tests/quotes.py
@@ -2,7 +2,6 @@ import pytest
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait

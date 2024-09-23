196dacafeed0ae5c671628747a96bf0d5d2e3009
dangoss76 <dangoss.perso@gmail.com>
2024-09-23 10:02:19 -0400

    added pandas to requirement51

--- test_quotes.py
+++ test_quotes.py
@@ -23,7 +23,7 @@ class TestExampleOne:
             for index, row in data.iterrows():
                 # element = browser.find_element(By.ID,'page-content') # Find the top element of the page
                 # browser.execute_script("arguments[0].scrollIntoView();", element) # We scroll back to top
-                self.driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + Keys.HOME)
+                #self.driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + Keys.HOME)
                 dropdown_equity = self.driver.find_element(By.ID, (f'{q1}'))  # we select a quotes in the dropdown
                 select = Select(dropdown_equity)
                 select.select_by_value(row['symbol'])
